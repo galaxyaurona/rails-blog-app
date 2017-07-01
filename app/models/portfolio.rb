bc46fc1f104e3f38aa96c93eea9823b1f611f716
Neil Tran <galaxyaurona@gmail.com>
2017-07-01 13:21:29 +0000

    add technologies

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,12 +1,17 @@
 class Portfolio < ApplicationRecord
     include Placeholder
-    validates_presence_of :title, :subtitle, :body
+ 
+    has_many :technologies
+    accepts_nested_attributes_for   :technologies, 
+                                    reject_if: lambda { |attrs| attrs["name"].blank?}
+       validates_presence_of :title, :subtitle, :body    
      # create an instance method on self
     def self.angular
         where(subtitle: "angular")
     end
   
     scope :ruby, -> { where(subtitle: "Ruby") }
+    
     after_initialize :set_defaults
     
     def set_defaults
@@ -14,4 +19,5 @@ class Portfolio < ApplicationRecord
         self.main_image ||= Placeholder.image_generator(height: 400, width: 600)
         self.thumb_image ||= Placeholder.image_generator(height: 200, width: 350)
     end
+
 end
